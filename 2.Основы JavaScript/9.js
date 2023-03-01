805fbc66f6e8f7725da1d704fa43435daf48ae72
Mother-investment <111118664+Mother-investment@users.noreply.github.com>
2023-03-01 09:16:03 +0700

    Update 9.js

--- 2.Основы JavaScript/9.js
+++ 2.Основы JavaScript/9.js
@@ -1,3 +1,4 @@
+//2
 let value = prompt('Какое "официальное" название JavaScript?', '')
 
 if (value == 'ECMAScript') {
@@ -7,7 +8,7 @@ if (value == 'ECMAScript') {
 }
 
 
-
+//3
 let value2 = prompt('Введите число', 0)
 
 if (value > 0) {
@@ -19,9 +20,11 @@ if (value > 0) {
 }
 
 
+//4
 let result = (a + b < 4) ? 'Мало' : 'Много'
 
 
+//4
 let message = (login == 'Сотрудник') ? 'Привет' :
 			  (login == 'Директор') ? 'Здравствуйте' :
 			  (login == '') ? 'Нет логина' : ''
